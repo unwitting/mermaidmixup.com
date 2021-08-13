621cb13d9c49c8472b0f2bf8d44ef8ead63a659f
Jack Preston <unwttng@gmail.com>
2021-08-13 18:56:52 +0100

    Etsy API calls now "single-threaded"

--- src/utils/etsy.js
+++ src/utils/etsy.js
@@ -4,13 +4,15 @@ import wait from "wait";
 
 dotenv.config();
 
-const { ETSY_API_KEY, ETSY_SHOP_ID, ETSY_USE_DEMO_DATA } = process.env;
+const { ETSY_API_KEY, ETSY_SHOP_ID } = process.env;
 
-let activeListingsCache = null;
+const getActiveListings = async () => {
+  activeListings.running = true;
 
-export const getActiveListings = async () => {
-  if (activeListingsCache) {
-    return activeListingsCache;
+  console.log("Getting active Etsy listings from API...");
+
+  if (activeListings.cache) {
+    return activeListings.cache;
   }
 
   const resp = await fetch(
@@ -19,16 +21,41 @@ export const getActiveListings = async () => {
   const { results: listings } = await resp.json();
 
   for (const listing of listings) {
-    console.log(`Getting images for listing ${listing.listing_id}`);
-    await wait(1000);
+    // console.log(`Getting images for listing ${listing.listing_id}`);
+    await wait(400);
     const imageResp = await fetch(
       `https://openapi.etsy.com/v3/application/shops/${ETSY_SHOP_ID}/listings/${listing.listing_id}/images?client_id=${ETSY_API_KEY}`
     );
     const { count, results: images } = await imageResp.json();
-    console.log(`Got ${count} images`);
+    console.log(`Got ${count} images for listing ${listing.listing_id}`);
     listing.images = images;
   }
 
-  activeListingsCache = listings;
+  console.log("Finished getting active Etsy listings");
+
+  activeListings.cache = listings;
+  activeListings.running = false;
+
   return listings;
+};
+
+export const activeListings = {
+  cache: null,
+  running: false,
+  init() {
+    if (!this.running && !this.cache) {
+      // console.log("First init(): calling getActiveListings()");
+      getActiveListings();
+    } else {
+      // console.log(
+      //   "Subsequent init(): getActiveListings() already run / running"
+      // );
+    }
+  },
+  async get() {
+    while (this.running) {
+      await wait(1000);
+    }
+    return this.cache;
+  },
 };
