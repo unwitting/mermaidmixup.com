ad197f64e404c193104ef8053df89355459040f7
Juniper Preston <unwttng@gmail.com>
2022-07-12 19:21:04 +0100

    More progress

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
 const eleventySass = require("eleventy-sass");
+const markdownIt = require("markdown-it");
+
+const md = new markdownIt({
+  html: true,
+});
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPlugin(eleventySass);
+
+  eleventyConfig.addFilter("markdown", (content) => {
+    return md.render(content);
+  });
 };
