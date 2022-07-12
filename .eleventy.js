36d5a1eaab1ef968cf8e9914ae60ed21027a3300
Juniper Preston <unwttng@gmail.com>
2022-07-12 21:53:14 +0100

    Proper SEO properties for listing index and listings pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 const eleventySass = require("eleventy-sass");
 const markdownIt = require("markdown-it");
 
+const { baseURL } = require("./_data/site")();
+
 const md = new markdownIt({
   html: true,
 });
@@ -11,5 +13,11 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter("markdown", (content) => {
     return md.render(content);
+  });
+  eleventyConfig.addFilter("prefixURLIfNotAbsolute", (urlOrPath) => {
+    if (urlOrPath.startsWith("http")) {
+      return urlOrPath;
+    }
+    return `${baseURL}${urlOrPath}`;
   });
 };
