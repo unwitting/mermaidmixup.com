3f337748868daeb46c5e6843b5ee666224c34ecb
Juniper <unwttng@gmail.com>
2022-07-12 21:08:26 +0100

    Listing pages (#8)
    * Get old code up and running
    
    * Return data from a dev cache if not configured for live access
    
    * More progress
    
    * Layout with image
    
    * Spacing tweak
    
    * Disable Etsy API communication in non-production environments

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,15 @@
 const eleventySass = require("eleventy-sass");
+const markdownIt = require("markdown-it");
+
+const md = new markdownIt({
+  html: true,
+});
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPlugin(eleventySass);
 
-  eleventyConfig.addCollection("sitemapPages", function (collectionApi) {
-    return collectionApi.getAllSorted().filter(function (item) {
-      const extension = item.inputPath.split(".").pop();
-      return (
-        extension === "njk" ||
-        (extension === "md" && !item.inputPath.endsWith("README.md"))
-      );
-    });
+  eleventyConfig.addFilter("markdown", (content) => {
+    return md.render(content);
   });
 };
