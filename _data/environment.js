285078bd62c8c5f7ee4cc0753a30ffb99a2a1e96
Juniper Preston <unwttng@gmail.com>
2022-07-12 10:27:48 +0100

    Proper environment and site configs for different Netlify envs

--- _data/environment.js
+++ _data/environment.js
@@ -1,10 +1,10 @@
-const ENVIRONMENT =
-  process.env.MERMAID_MIXUP_ENVIRONMENT === "production"
-    ? "production"
-    : "development";
+const ENVIRONMENT = process.env.MERMAID_MIXUP_ENVIRONMENT || "development";
 
 module.exports = () => ({
   environment: ENVIRONMENT,
   isProduction: ENVIRONMENT === "production",
+  isProductionPreview: ENVIRONMENT === "production-preview",
+  isProductionLike:
+    ENVIRONMENT === "production" || ENVIRONMENT === "production-preview",
   isDevelopment: ENVIRONMENT === "development",
 });
--- _data/site.js
+++ _data/site.js
@@ -1,11 +1,45 @@
-const { isProduction } = require("./environment");
+const { isProduction, isDevelopment, isProductionPreview } =
+  require("./environment")();
 
-const protocol = isProduction ? "https" : "http";
-const domain = isProduction ? "mermaidmixup.com" : "localhost:8080";
-const baseURL = `${protocol}://${domain}`;
+console.log(isDevelopment);
+
+const getProtocol = () => {
+  if (isProduction) {
+    return "https";
+  }
+  if (isDevelopment) {
+    return "http";
+  }
+  if (isProductionPreview) {
+    const url = new URL(process.env.DEPLOY_URL);
+    return url.protocol.slice(0, -1);
+  }
+};
+
+const getDomain = () => {
+  if (isProduction) {
+    return "mermaidmixup.com";
+  }
+  if (isDevelopment) {
+    return "localhost:8080";
+  }
+  if (isProductionPreview) {
+    const url = new URL(process.env.DEPLOY_URL);
+    return url.hostname;
+  }
+};
+
+const getBaseURL = () => {
+  if (!isProductionPreview) {
+    return `${getProtocol()}://${getDomain()}`;
+  }
+  return process.env.DEPLOY_URL;
+};
 
 module.exports = () => ({
-  protocol,
-  domain,
-  baseURL,
+  protocol: getProtocol(),
+  domain: getDomain(),
+  baseURL: getBaseURL(),
 });
+
+console.log(module.exports());
