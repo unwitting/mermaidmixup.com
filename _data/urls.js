09aad93f7da4a702df0e6b89491d7472cd5d8ee8
Juniper Preston <unwttng@gmail.com>
2022-07-13 11:02:39 +0100

    Add /pay-what-you-can. Closes #4.

--- _data/urls.js
+++ _data/urls.js
@@ -6,5 +6,7 @@ module.exports = {
       "https://www.etsy.com/shop/mermaidmixup?utm_source=mermaidmixup.com&utm_medium=referral&utm_campaign=mermaidmixup-footer",
     indexPage:
       "https://www.etsy.com/shop/mermaidmixup?utm_source=mermaidmixup.com&utm_medium=referral&utm_campaign=mermaidmixup-indexpage",
+    payWhatYouCanPage:
+      "https://www.etsy.com/shop/mermaidmixup?utm_source=mermaidmixup.com&utm_medium=referral&utm_campaign=mermaidmixup-paywhatyoucanpage",
   },
 };
