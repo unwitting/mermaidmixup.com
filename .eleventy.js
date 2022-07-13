b36a01f23ae7520ea98b1f14403fe42dc107cde4
Juniper Preston <unwttng@gmail.com>
2022-07-13 10:12:43 +0100

    Final RSS output

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,13 @@
 const eleventySass = require("eleventy-sass");
 const markdownIt = require("markdown-it");
+const markdownItPlainText = require("markdown-it-plain-text");
 
 const { baseURL } = require("./_data/site")();
 
 const md = new markdownIt({
   html: true,
 });
+md.use(markdownItPlainText);
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
@@ -13,6 +15,10 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter("markdown", (content) => {
     return md.render(content);
+  });
+  eleventyConfig.addFilter("markdownToPlainText", (content) => {
+    md.render(content);
+    return md.plainText;
   });
   eleventyConfig.addFilter("prefixURLIfNotAbsolute", (urlOrPath) => {
     if (urlOrPath.startsWith("http")) {
